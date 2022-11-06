e0a9072908d63c3ec866db5fd77bd5a57f1e5e4f
HuyGriffiths <115134088+huyg610@users.noreply.github.com>
2022-11-06 01:35:40 -0800

    Added Hero Animations
    changes:
    - added Hero animations (idle, right, left, up). Missing walk_right animation, will add later.
    - commented out code in PlayerMovement script to use a different script that works with Animations.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,18 +6,40 @@ public class PlayerMovement : MonoBehaviour
     public static float playerHealth = 10;
     public static float playerXP = 0;
 
-
+    public Animator animator;
+    public Rigidbody2D rb;
+    Vector2 movement;
 
     public KeyCode jumpKey;
     public float speed = 10f;
+    
     void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+
+    // commented this script out for now, because I wrote a different movement script to make Animations work - Huy
+    /*void Update()
     {
         if(playerHealth <= 0){
             // Game over screen
         }
 
-
         WASD_Movement();
+
+        
     }
 
     private void WASD_Movement(){
@@ -25,19 +47,24 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey("w"))
         {
             pos.y += speed * Time.deltaTime;
+
         }
         if (Input.GetKey("s"))
         {
             pos.y -= speed * Time.deltaTime;
+
         }
         if (Input.GetKey("d"))
         {
             pos.x += speed * Time.deltaTime;
+
         }
         if (Input.GetKey("a"))
         {
             pos.x -= speed * Time.deltaTime;
+
         }
         transform.position = pos;
     }
+    */
 }
