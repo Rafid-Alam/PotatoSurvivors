aa3c24270ee1654e38a55dbf39660db07619b46b
Huy Griffiths <huyvn111@gmail.com>
2022-11-07 14:26:05 -0800

    added more animations and music
    - finished player walking animations
    - added walking potato sprite animations
    - added some backgound music

--- Assets/Scripts/Enemy1/Enemy1Movement.cs
+++ Assets/Scripts/Enemy1/Enemy1Movement.cs
@@ -6,6 +6,7 @@ public class Enemy1Movement : MonoBehaviour
 {
     private GameObject player;
     public float speed = 5f;
+    public bool flip;
     void Start()
     {
         player = GameObject.Find("Hero");
@@ -16,6 +17,18 @@ public class Enemy1Movement : MonoBehaviour
         transform.position = Vector2.MoveTowards(transform.position,
         player.transform.position, speed * Time.deltaTime);
         //transform.position = transform.position.MoveTowards(transform.position, player.transform.position, 5);
+
+        Vector3 scale = transform.localScale;
+        if (player.transform.position.x > transform.position.x) // makes enemy face player
+        {
+            scale.x = Mathf.Abs(scale.x) * -1 * (flip ? -1 : 1);
+
+        }
+        else
+        {
+            scale.x = Mathf.Abs(scale.x) * (flip ? -1 : 1);
+        }
+        transform.localScale = scale;
     }
     void OnCollisionEnter2D(Collision2D col){
         // Different enemies will deal different damage
