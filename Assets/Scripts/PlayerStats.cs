78382c3af3fdcf9fcd1d4cd84c958b8555f94c8f
HuyGriffiths <115134088+huyg610@users.noreply.github.com>
2022-11-12 20:53:43 -0800

    Added Main Menu & Game Over  scenes
    - Added a MainMenu scene (with PLAY / OPTION / QUIT buttons)
    - Added a GameOver scene (which loads when playerhealth < 0) (with RESTART / QUIT buttons)
    - PLAY and QUIT buttons will transition between the scenes.
    - BUG: when you hit restart, player health still remain 0.

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -29,6 +31,7 @@ public class PlayerStats : MonoBehaviour
         if(health <= 0){
             //Game Over
             //Destroy (this.gameObject);
+            SceneManager.LoadScene("GameOver");
         }
         
     }
