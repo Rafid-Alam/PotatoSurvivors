3f8cc5afdd8a342c4acb6ceb6598a88f8435b12d
HuyGriffiths <115134088+huyg610@users.noreply.github.com>
2022-11-15 22:04:57 -0800

    Revert "Revert "Revert "PAUSE MENU UNFINISHED"""
    This reverts commit 8d8b234b1ed02c62c61e8c38a75f39d73a4436aa.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< Updated upstream
 
 public class PauseMenu : MonoBehaviour
 {
@@ -18,77 +17,21 @@ public class PauseMenu : MonoBehaviour
             } else
             {
                 Pause();
-=======
-using UnityEngine.SceneManagement;
-
-public class PauseMenu : MonoBehaviour
-{
-    public GameObject pauseMenu;
-
-    public static bool isPaused;
-
-    void Start()
-    {
-        pauseMenu.SetActive(false);
-    }
-
-
-
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            if(isPaused)
-            {
-                Debug.Log("UNPAUSE");
-                ResumeGame();
-            }
-            else
-            {
-                PauseGame();
-                Debug.Log("PAUSE");
->>>>>>> Stashed changes
             }
         }
     }
 
-<<<<<<< Updated upstream
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
-=======
-    public void PauseGame()
-    {
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0f;
-        isPaused = true;
-    }
-
-    public void ResumeGame()
-    {
-        pauseMenu.SetActive(false);
->>>>>>> Stashed changes
         Time.timeScale = 1f;
         isPaused = false;
     }
 
-<<<<<<< Updated upstream
     private void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
-=======
-    public void GoToMainMenu()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("MainMenu");
-        isPaused = false;
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
->>>>>>> Stashed changes
     }
 }
