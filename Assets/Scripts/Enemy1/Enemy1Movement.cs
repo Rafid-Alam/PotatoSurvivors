046e9af61e57769002e2585bd0c8ed6d94ea2b91
HuyGriffiths <115134088+huyg610@users.noreply.github.com>
2022-11-16 11:53:04 -0800

    Revert "Locked enemy rotation"
    This reverts commit 9f42ff47e468491823e21b1026d98de9ac5f4da9.

--- Assets/Scripts/Enemy1/Enemy1Movement.cs
+++ Assets/Scripts/Enemy1/Enemy1Movement.cs
@@ -8,7 +8,6 @@ public class Enemy1Movement : MonoBehaviour
     public float speed = 5f;
     public bool flip;
     public float damage = 5f;
-    public float lockPos = 0;
     void Start()
     {
         player = GameObject.Find("Hero");
@@ -20,7 +19,6 @@ public class Enemy1Movement : MonoBehaviour
         player.transform.position, speed * Time.deltaTime);
         //transform.position = transform.position.MoveTowards(transform.position, player.transform.position, 5);
 
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, lockPos, lockPos); // lock axis rotations so enemies only walk sideways
         Vector3 scale = transform.localScale;
         if (player.transform.position.x > transform.position.x) // makes enemy face player
         {
